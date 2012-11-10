15f189947ee1e2f78c545b6b82c268a1573669f1
Ronald Brown <ron.brown@gatech.edu>
2012-11-10 15:28:40 -0500

    Added NFA.java, updated State.java

---
+++ Source/NFA.java
@@ -0,0 +1,105 @@
+package Source;
+
+/**
+ * 
+ * @author Ron brown
+ *
+ */
+public class NFA {
+	//Variables
+	public State entry;
+	public State exit;
+	
+	/**
+	 * Set a up  NFA 'Node'
+	 * @param entry
+	 * @param exit
+	 */
+	public NFA(State entry, State exit) {
+		this.entry = entry;
+		this.exit = exit;
+	}
+	
+	/**
+	 * Create node with char transition
+	 * @param c
+	 * @return
+	 */
+	public NFA createChar(char c) {
+		State etnry = new State();
+		State exit = new State();
+		exit.isFinal = true;
+		entry.addCharEdge(c, exit);
+		
+		return new NFA(entry, exit);
+	}
+	
+	/**
+	 * Create epislon transition
+	 * @return
+	 */
+	public NFA epsilon() {
+		State entry = new State();
+		State exit = new State();
+		
+		entry.addepsilonEdge(exit);
+		exit.isFinal = true;
+		return new NFA(entry, exit);
+		
+	}
+	
+	/**
+	 * Creates NFA that matches 0 or more, i.e. a*
+	 * @param nfa
+	 * @return
+	 */
+	public NFA repetition(NFA nfa) {
+		nfa.exit.addepsilonEdge(nfa.entry);
+		nfa.entry.addepsilonEdge(nfa.exit);
+		
+		return nfa;
+		
+	}
+	
+	/**
+	 * Creates an NFA that matches a sequence
+	 * @param first
+	 * @param next
+	 * @return
+	 */
+	public NFA sequence(NFA first, NFA next) {
+		first.exit.isFinal = false;
+		next.exit.isFinal = true;
+		first.exit.addepsilonEdge(next.entry);
+		return new NFA(first.entry, next.exit);
+	}
+	
+	/**
+	 * Creates an NFA that matches the OR operator
+	 * @param top
+	 * @param bottom
+	 * @return
+	 */
+	public NFA or(NFA top, NFA bottom) {
+		top.exit.isFinal = false;
+		bottom.exit.isFinal = false;
+		
+		State entry = new State();
+		State exit = new State();
+		
+		exit.isFinal = true;
+		entry.addepsilonEdge(top.entry);
+		entry.addepsilonEdge(bottom.entry);
+		
+		top.exit.addepsilonEdge(exit);
+		bottom.exit.addepsilonEdge(exit);
+		return new NFA(entry, exit);
+		
+	}
+	
+	
+	
+	
+	
+
+}
--- Source/State.java
+++ Source/State.java
@@ -1,5 +1,38 @@
 package Source;
+
+import java.util.*;
+
 public class State {
 	int stateNum;
 	boolean isFinal;
+	
+	private ArrayList<State> oChar[] = new ArrayList[255];
+	private ArrayList<State> oEmpty = new ArrayList();
+	
+	public void addepsilonEdge(State next) {
+		
+	}
+	
+	public void addCharEdge(char c, State next) {
+		
+	}
+	
+	public State() {
+		
+	}
+	
+	/*public boolean match(String s) {
+		
+		return matches(s, new ArrayList());
+	}
+	
+	private boolean matches(String s, ArrayList visited) {
+		
+		return false;
+	}
+	*/
+	
+	
+	
+	
 }
