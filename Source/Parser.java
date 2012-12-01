af7d21f1e1593a0639b88a3141998b6637cff04e
Ronald Brown <ron.brown@gatech.edu>
2012-12-01 12:52:48 -0500

    Update to Parser, making notes on token types and required parsing.
    //IDorKey
    //ID
    //KEY
    //REGEX
    //ASCII

--- Source/Parser.java
+++ Source/Parser.java
@@ -1,5 +1,16 @@
 package Source;
 
+//Token Types:
+
+//IDorKey
+//ID
+//KEY
+//REGEX
+//ASCII
+
+
+
+
 
 /**
  * Parser Class
@@ -8,6 +19,16 @@ package Source;
  */
 public class Parser {
 	
+	//Variables
+	private ScannerGenerator scanner;
+	
+	//Constructors
+	public Parser(ScannerGenerator scanner) {
+		this.scanner = scanner;
+	}
+	
+	
+	//Methods
 	/**
 	 * Validates the length of the ID within a token
 	 * @param t
@@ -28,4 +49,16 @@ public class Parser {
 		return true;
 	}
 
+	
+	//Adding two more functions
+	//token name will be IDorKey
+	//check keywords list, if is in keyword list then change type to keyword
+	//otherwise make it an id token
+	//if idToken, call verifyIDFormat (this function makes sure it starts with a letter and is length > 1 and < 10 
+	//is a letter, digit, and underscore
+	//if keytoken, change the type to keyword
+	
+	
+	//Function to check if token is keyword
+	//Function to check if token is id
 }
