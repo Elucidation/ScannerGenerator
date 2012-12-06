456422d25508f987075fba3497f6628c79ac6e5f
David Sharpe <shellsonthefloor@gmail.com>
2012-12-06 17:37:41 -0500

    Added more operations

--- Source/Operations.java
+++ Source/Operations.java
@@ -5,10 +5,35 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Operations {
+	
+	public static void main(String[] args){
+		Operations o = new Operations();
+		String[] a = o.find("[A-Za-z]+e[ |\t|\n]","C:\\repos\\scannergenerator\\Samples\\regexTest.txt");
+		String[] b = o.find("t[a-zA-Z]*[ |\t|\n]","C:\\repos\\scannergenerator\\Samples\\regexTest.txt");
+		String[] c = o.intersec(a,b);
+		for(int i = 0;i<a.length;i++){
+			System.out.println(a[i]);
+		}		
+		System.out.println("And thus ends the words ending in e");
+
+		for(int i = 0;i<b.length;i++){
+			System.out.println(b[i]);
+
+		}
+		System.out.println("And thus ends the words starting in t");
+
+		for(int i = 0;i<c.length;i++){
+			System.out.println(c[i]);
+		}
+	}
+	
+	
 	
 	public Operations(){
 		
@@ -64,6 +89,67 @@ public class Operations {
 			return false;		
 		}
 		return true;
+	}
+	
+	public String[] find(String regex, String infile){
+		File in = new File(infile);
+		FileInputStream inFile;
+		try {
+			inFile = new FileInputStream(in);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		StringBuffer inputFileText = new StringBuffer();
+		char c;
+		try {
+			c = (char) inFile.read();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return null;
+		}
+
+		while(c != (char)65535){
+			inputFileText.append(c);	
+			try {
+				c = (char) inFile.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		Pattern regexPattern = Pattern.compile(regex);
+	    Matcher matcher = regexPattern.matcher(inputFileText.toString());
+	    ArrayList<String> a = new ArrayList<String>();
+	    while(matcher.find()){
+	    	a.add(matcher.group());
+	    }
+		String[] results = new String[a.size()];
+		return   a.toArray(results);
+	}
+	
+	public String[] intersec(String[] a, String[] b){
+
+		HashSet<String> hs = new HashSet<String>();
+		for(int i=0;i<a.length;i++){
+			hs.add(a[i]);
+		}
+		HashSet<String> rs = new HashSet<String>();
+		for(int i=0;i<b.length;i++){
+			if(hs.contains(b[i])){
+				rs.add(b[i]);
+			}
+		}
+		
+		String[] results = new String[rs.size()];
+		return rs.toArray(results);
+	}
+	
+	public int hash(String[] a){
+		return a.length;
+	}
+	public void print(int a){
+		System.out.println(a);
 	}
 
 }
